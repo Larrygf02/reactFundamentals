489268f19208bae2f44b0463c5949f2a25aafa58
larrygf02 <raulgf_02@hotmail.com>
2019-03-28 21:40:13 -0500

    Add method onClick

--- src/App.js
+++ src/App.js
@@ -9,12 +9,17 @@ class App extends Component {
       {name: 'Kelly', age: 25}
     ]
   }
+
+  switchNameHandler = () => {
+    console.log('Clicked')
+  }
   render() {
     return (
       <div className="App">
         <h1>
           Hi, I'am React App
         </h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <p>This is really working</p>
         <Person name={this.state.persons[0].name} years={this.state.persons[0].age}></Person>
         <Person name={this.state.persons[1].name} years={this.state.persons[1].age}>My hobbies</Person>
