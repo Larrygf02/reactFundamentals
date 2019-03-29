d4952beb84587129c608beac3171ebdd2456bba1
larrygf02 <raulgf_02@hotmail.com>
2019-03-28 20:34:50 -0500

    Add content dinamic

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
         </h1>
         <p>This is really working</p>
         <Person></Person>
+        <Person></Person>
       </div>
       );
       //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I am react app'))
