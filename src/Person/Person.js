020216e10fd31c3298e79ecc360df201032a2c6b
larrygf02 <raulgf_02@hotmail.com>
2019-03-30 18:49:39 -0500

    Using css in components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Person.css'
  
 const Person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'am {props.name} and I am {props.years} years old</p>
             <span>{props.children}</span>
             <input type="text" onChange={props.changed} value={props.name}></input>
