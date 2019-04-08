47da6e90c5c7e5ddbd95dde4b9d77b9b345f9a84
larrygf02 <raulgf_02@hotmail.com>
2019-04-08 00:43:33 -0500

    Error Boundary

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from './Person.css'   
  
 const Person = (props) => {
-
     return (
         <div className={styles.Person}>
             <p onClick={props.click}>I'am {props.name} and I am {props.years} years old</p>
