bb1f2295cac3e8ea0eb23072654ad27452bcd2ae
larrygf02 <raulgf_02@hotmail.com>
2019-04-16 21:09:32 -0500

    Use effect when change props persons

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -2,9 +2,15 @@ import React, {useEffect } from 'react'
 import styles from './Cockpit.css'
 
 const cockpit = (props) => {
+    //El props.persons indica que se ejecutara si cambia el props persons
+    //sino se ejecutara cada vez que haya un cambio
     useEffect(() => {
       console.log('[Cockpit.js] useEffect');
-    });
+      //Http Request
+      setTimeout(() => {
+        alert('Save data cloud')
+      }, 1000)
+    }, [props.persons]);
     let classes = []
     let btnClass = ''
     if (props.showPersons) {
