1373cdce3748cc6e7824d913225d1e4df43812b1
larrygf02 <raulgf_02@hotmail.com>
2019-04-16 20:59:49 -0500

    Use effect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, {useEffect } from 'react'
 import styles from './Cockpit.css'
 
 const cockpit = (props) => {
+    useEffect(() => {
+      console.log('[Cockpit.js] useEffect');
+    });
     let classes = []
     let btnClass = ''
     if (props.showPersons) {
