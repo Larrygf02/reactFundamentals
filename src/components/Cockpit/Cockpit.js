c0905cafa78be87f785cde412aab93f4bbfe3974
larrygf02 <raulgf_02@hotmail.com>
2019-04-24 21:57:23 -0500

    React Memo with functional component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -26,10 +26,10 @@ const cockpit = (props) => {
     if (props.showPersons) {
       btnClass = styles.Red
     }
-    if (props.persons.length <= 2) {
+    if (props.personsLength <= 2) {
       classes.push(styles.red)
     }
-    if (props.persons.length <= 1) {
+    if (props.personsLength <= 1) {
       classes.push(styles.bold)
     }
     classes = classes.join(' ')
@@ -44,4 +44,5 @@ const cockpit = (props) => {
     );
 };
 
-export default cockpit;
+//rendering if change input
+export default React.memo(cockpit);
