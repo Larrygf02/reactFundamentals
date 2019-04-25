00ccb33f64d442139c41f53f259b64b1afb7a419
larrygf02 <raulgf_02@hotmail.com>
2019-04-24 22:31:21 -0500

    Rendering Adjacents jsx

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
-import styles from './Person.css'   
+import styles from './Person.css'
+import Aux from '../../../hoc/auxiliary'   
  
 class Person extends Component {
     render() {
         console.log('[Person.js rendering...]');
         return (
-            <div className={styles.Person}>
+            //<div className={styles.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I'am {this.props.name} and I am {this.props.years} years old</p>
                 <span>{this.props.children}</span>
                 <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </div>
+            </Aux>
+            //</div>
         )
     }
 }
