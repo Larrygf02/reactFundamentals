13e84363630e64bdd690df98fc7ec61c689df54c
larrygf02 <raulgf_02@hotmail.com>
2019-04-28 10:46:31 -0500

    Passing Props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import styles from './Person.css'
-import Aux from '../../../hoc/auxiliary'   
+import Aux from '../../../hoc/auxiliary'
+import withClass from '../../../hoc/withClass'
  
 class Person extends Component {
     render() {
@@ -17,4 +18,4 @@ class Person extends Component {
     }
 }
 
-export default Person;
+export default withClass(Person, styles.Person);
